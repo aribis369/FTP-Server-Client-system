03a6572ed808b88d10dab843708cea32cf60ebd4
aribis369 <aribis369@gmail.com>
2017-05-06 14:14:20 +0800

    comments added

--- client.py
+++ client.py
@@ -6,25 +6,33 @@ import os
 ftp=FTP()
 # setting connection with the server taking username and password as input from the user
 def conn():
+    # connecting to server ip address
     ftp.connect("localhost",8000)
+    # taking input from user
     user=input("Username:")
     passwrd=input("Password:")
+    # sending credentials for verification
     ftp.login(user,passwrd)
 
 # sending the server those files which are stared i.e. their name end with a star
 def send(fname):
+    # going to the specified directory
     os.chdir("/home/arindam/Desktop/c")
     q=open(fname,"rb")
     m=fname.replace("/home/arindam/Desktop/c/","")
     print m
     l="STOR "+m
+    # performing STOR command to upload file to server
     ftp.storbinary(l,q)
+    # changing file names that have been uploaded by replacing * in their names
     nf=m.replace("*","")
     os.rename(m,nf)
 
 #extractig paths of files in the directory and calling the send() function on them
 def exfiles():
+    # finding all files in the specified directory
     files=glob.glob("/home/arindam/Desktop/c/*.*")
+    # checking files with * at their end
     for f in files:
         print f
         k=f.index(".")
@@ -32,7 +40,8 @@ def exfiles():
         if ch=='*':
             send(f)
 
-#this module is used to get command from the user
+# this module is used to get command from the user
+# some error handling applied more to be applied
 def command():
     com=input("command>>>")
     if com=="upload":
